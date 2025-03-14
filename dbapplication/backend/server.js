98b3029a3b99ec58b4e5cf9bf0bbac530d017d5b
Tahbir Moon <tahbir.moon@centria.fi>
2025-03-14 02:55:07 +0200

    update

--- dbapplication/backend/server.js
+++ dbapplication/backend/server.js
@@ -3,11 +3,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
 const app = express();
 const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Connect to the SQLite database
 let db = new sqlite3.Database('my-database.db', (err) => {
